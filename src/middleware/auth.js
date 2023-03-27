dd3568de7e867562d048e20b9bfab1349e88a41c
Kaj Lund <kaj.lund@gmail.com>
2023-03-27 19:12:29 +0300

    Refactored for nicer error messages

---
+++ src/middleware/auth.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+
+const cnf = require('../config')
+const { UnauthorizedError } = require('../utils/errors')
+const logger = require('../utils/logger')
+const User = require('../api/users/user.model')
+
+exports.auth = async (req, res, next) => {
+  const authHeader = req.headers.authorization
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
+    throw new UnauthorizedError('Invalid token')
+  }
+  const token = authHeader.split(' ')[1]
+  try {
+    const decoded = jwt.verify(token, cnf.jwtAccessTokenSecret)
+    // attach the user to request
+    req.user = await User.findById(decoded.userId)
+    if (!req.user || !req.user.role) {
+      next(new UnauthorizedError('Confirmed users only'))
+    }
+    next()
+  } catch (err) {
+    logger.error(err)
+    next(new UnauthorizedError('Invalid token'))
+  }
+}
+
+exports.admin = (req, res, next) => {
+  if (req.user.role === 'admin') {
+    return next()
+  }
+  next(new UnauthorizedError('Admins only'))
+}
--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,42 +1,42 @@
 const { reasonPhrases, statusCodes } = require('./statuscodes')
 
 class AppError extends Error {
-  constructor(message = reasonPhrases.INTERNAL_SERVER_ERROR, status = statusCodes.INTERNAL_SERVER_ERROR, errors = []) {
+  constructor(message = reasonPhrases.INTERNAL_SERVER_ERROR, status = statusCodes.INTERNAL_SERVER_ERROR, detail = '') {
     super(message)
     this.status = status
     this.name = this.constructor.name
-    this.errors = errors
+    this.detail = detail
     Error.captureStackTrace(this, this.constructor)
   }
 }
 
 class BadRequestError extends AppError {
-  constructor(message = reasonPhrases.BAD_REQUEST, status = statusCodes.BAD_REQUEST, errors) {
-    super(message, status, errors)
+  constructor(detail) {
+    super(reasonPhrases.BAD_REQUEST, statusCodes.BAD_REQUEST, detail)
   }
 }
 
 class InternalServerError extends AppError {
-  constructor(message = reasonPhrases.INTERNAL_SERVER_ERROR, status = statusCodes.INTERNAL_SERVER_ERROR) {
-    super(message, status)
+  constructor(detail = '') {
+    super(reasonPhrases.INTERNAL_SERVER_ERROR, statusCodes.INTERNAL_SERVER_ERROR, detail)
   }
 }
 
 class MaximumFileSizeException extends AppError {
-  constructor(size = 100, status = statusCodes.BAD_REQUEST) {
-    super(`Maximum upload file size (${size}) exceeded`, status)
+  constructor(size = 100) {
+    super(reasonPhrases.BAD_REQUEST, statusCodes.BAD_REQUEST, `Maximum upload file size (${size}) exceeded`)
   }
 }
 
 class NotFoundError extends AppError {
-  constructor(message = reasonPhrases.NOT_FOUND, status = statusCodes.NOT_FOUND) {
-    super(message, status)
+  constructor(detail = '') {
+    super(reasonPhrases.NOT_FOUND, statusCodes.NOT_FOUND, detail)
   }
 }
 
 class UnauthorizedError extends AppError {
-  constructor(message = reasonPhrases.UNAUTHORIZED, status = statusCodes.UNAUTHORIZED) {
-    super(message, status)
+  constructor(detail = '') {
+    super(reasonPhrases.UNAUTHORIZED, statusCodes.UNAUTHORIZED, detail)
   }
 }
 
